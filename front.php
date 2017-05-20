a9814f0c79739221e7b5cac184621f1682d6915e
BLARY Jason <jason.blary@pictime.com>
2017-05-20 02:54:15 +0200

    trucs

--- front.php
+++ front.php
@@ -2,6 +2,7 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="front.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 
